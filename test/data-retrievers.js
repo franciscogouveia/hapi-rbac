be6fb438ec99c71787297cce2f69af41416cb6da
Francisco de Gouveia <francisco@de-gouveia.eu>
2016-01-31 21:37:03 +0100

    Refactored data retrieval tests to use the DataRetrievalRouter object instead

--- test/data-retrievers.js
+++ test/data-retrievers.js
@@ -5,6 +5,7 @@ const Boom = require('boom');
 const Code = require('code');
 const Lab = require('lab');
 const Rbac = require('../');
+const DataRetrievalRouter = Rbac.DataRetrievalRouter;
 
 const lab = exports.lab = Lab.script();
 const experiment = lab.experiment;
@@ -18,87 +19,89 @@ const expect = Code.expect;
  **/
 experiment('RBAC internal modular information retrieval', () => {
 
+    const dataRetriever = new DataRetrievalRouter();
+
     test('should register a valid retriever', (done) => {
 
-        const retriever = (source, key, request) => {
+        const retriever = (source, key, context) => {
             return 'key-' + key;
         };
 
-        Rbac.registerDataRetriever('test', retriever);
+        dataRetriever.register('test', retriever);
 
-        expect(Rbac.retrieveData('test:x', {})).to.equal('key-x');
+        expect(dataRetriever.get('test:x')).to.equal('key-x');
 
         done();
     });
 
     test('should override a valid retriever (single handler)', (done) => {
 
-        const retriever1 = (source, key, request) => {
+        const retriever1 = (source, key, context) => {
             return key + '-1';
         };
 
-        const retriever2 = (source, key, request) => {
+        const retriever2 = (source, key, context) => {
             return key + '-2';
         };
 
-        Rbac.registerDataRetriever('test-override', retriever1);
-        Rbac.registerDataRetriever('test-override', retriever2, {override: true});
+        dataRetriever.register('test-override', retriever1);
+        dataRetriever.register('test-override', retriever2, {override: true});
 
-        expect(Rbac.retrieveData('test-override:test', {})).to.equal('test-2');
+        expect(dataRetriever.get('test-override:test', {})).to.equal('test-2');
 
         done();
     });
 
     test('should not override a valid retriever (single handler)', (done) => {
 
-        const retriever1 = (source, key, request) => {
+        const retriever1 = (source, key, context) => {
             return key + '-1';
         };
 
-        const retriever2 = (source, key, request) => {
+        const retriever2 = (source, key, context) => {
             return key + '-2';
         };
 
-        Rbac.registerDataRetriever('test-override-error', retriever1);
+        dataRetriever.register('test-override-error', retriever1);
 
-        expect(Rbac.registerDataRetriever.bind(null, 'test-override-error', retriever2)).to.throw();
+        expect(dataRetriever.register.bind(dataRetriever, 'test-override-error', retriever2)).to.throw();
 
         done();
     });
 
     test('should override a valid retriever (multiple handlers)', (done) => {
 
-        const retriever1 = (source, key, request) => {
+        const retriever1 = (source, key, context) => {
             return key + '-1';
         };
 
-        const retriever2 = (source, key, request) => {
+        const retriever2 = (source, key, context) => {
             return key + '-2';
         };
 
-        Rbac.registerDataRetriever(['test-override-multiple-1', 'test-override-multiple-2', 'test-override-multiple-3'], retriever1);
-        Rbac.registerDataRetriever(['test-override-multiple-2', 'test-override-multiple-4'], retriever2, {override: true}); // test-override-multiple-2 collides
+        dataRetriever.register(['test-override-multiple-1', 'test-override-multiple-2', 'test-override-multiple-3'], retriever1);
+        dataRetriever.register(['test-override-multiple-2', 'test-override-multiple-4'], retriever2, {override: true}); // test-override-multiple-2 collides
 
-        expect(Rbac.retrieveData('test-override-multiple-1:test', {})).to.equal('test-1');
-        expect(Rbac.retrieveData('test-override-multiple-2:test', {})).to.equal('test-2');
-        expect(Rbac.retrieveData('test-override-multiple-3:test', {})).to.equal('test-1');
-        expect(Rbac.retrieveData('test-override-multiple-4:test', {})).to.equal('test-2');
+        expect(dataRetriever.get('test-override-multiple-1:test', {})).to.equal('test-1');
+        expect(dataRetriever.get('test-override-multiple-2:test', {})).to.equal('test-2');
+        expect(dataRetriever.get('test-override-multiple-3:test', {})).to.equal('test-1');
+        expect(dataRetriever.get('test-override-multiple-4:test', {})).to.equal('test-2');
 
         done();
     });
 
     test('should not override a valid retriever (multiple handlers)', (done) => {
 
-        const retriever1 = (source, key, request) => {
+        const retriever1 = (source, key, context) => {
             return key + '-1';
         };
 
-        const retriever2 = (source, key, request) => {
+        const retriever2 = (source, key, context) => {
             return key + '-2';
         };
 
-        Rbac.registerDataRetriever(['test-override-error-multiple-1', 'test-override-error-multiple-2', 'test-override-error-multiple-3'], retriever1);
-        expect(Rbac.registerDataRetriever.bind(null, ['test-override-error-multiple-2', 'test-override-error-multiple-4'], retriever2)).to.throw(Error, 'There is a data retriever already registered for the source: test-override-error-multiple-2');
+        dataRetriever.register(['test-override-error-multiple-1', 'test-override-error-multiple-2', 'test-override-error-multiple-3'], retriever1);
+        expect(dataRetriever.register.bind(dataRetriever, ['test-override-error-multiple-2', 'test-override-error-multiple-4'], retriever2)).to.throw(Error, 'There is a data retriever already registered for the source: test-override-error-multiple-2');
 
         done();
     });
